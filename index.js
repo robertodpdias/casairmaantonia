18160e3aba5a79b98e2b3e3b0ba04ff8180c30bf
Roberto D Pereira Dias <roberto@newlan.inf.br>
2022-04-08 10:12:00 -0300

    agora vai

--- index.js
+++ index.js
@@ -11,18 +11,13 @@ const app = Express()
 
 app.use(bodyParser.json({limit:"150mb"}))
 app.use(Express.json({limit:"150mb"}))
-/*app.use((req,res,next) =>{
-
-    res.header("Access-Control-Allow-Origin", "*")
-    req.header("Access-Control-Allow-Origin", "*")
-    req.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
-    app.use(cors())
-    next()
-})*/
 
 app.use(cors())
 app.use(route)
 
-app.listen(process.env.APP_PORT, () => {
-    console.log("Servidor rodando na porta 3000")
-})
+// app.listen(process.env.APP_PORT, () => {
+//     console.log("Servidor rodando na porta 3000")
+// })
+
+var porta = process.env.PORT || 8080;
+app.listen(porta);
